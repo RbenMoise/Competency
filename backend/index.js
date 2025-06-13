66ad640f98ef1a26815e713cbbe6fd6adca0b4fb
RbenMoise <reubenmoses91@gmail.com>
2025-06-13 15:18:24 +0300

    index

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "https://nockcompetency-qnwln.ondigitalocean.app",
-    origin: "http://localhost:3000",
+    origin: "https://nockcompetency-qnwln.ondigitalocean.app",
+    // origin: "http://localhost:3000",
     credentials: true,
   })
 );

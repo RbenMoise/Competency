1f9a8dc6c11c73fc37ecc6ed992fdc7a0b6bb217
RbenMoise <reubenmoses91@gmail.com>
2025-06-13 17:02:57 +0300

    n

--- backend/routes/scoreRoute.js
+++ backend/routes/scoreRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const ScoreSubmission = require("../models/scoreSubmittion");
-const Notification = require("../models/notification");
+const Notification = require("../models/Notification");
 
 router.post("/scores", async (req, res) => {
   const user = req.session.user;

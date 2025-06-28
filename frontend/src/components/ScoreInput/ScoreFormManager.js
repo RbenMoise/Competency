398f56cc8c1bd7f28f93d8b9350cd08746fb8c7c
RbenMoise <reubenmoses91@gmail.com>
2025-06-28 23:14:21 +0300

    data managment added

--- frontend/src/components/ScoreInput/ScoreFormManager.js
+++ frontend/src/components/ScoreInput/ScoreFormManager.js
@@ -51,7 +51,11 @@ export default function ScoreFormManager({
 
     try {
       const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/api/scores`,
+        `${
+          process.env.REACT_APP_API_URL
+            ? process.env.REACT_APP_API_URL
+            : "http://localhost:4000"
+        }/api/scores`,
         { current: currentScores, projected: projectedScores },
         { withCredentials: true }
       );
